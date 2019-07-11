baaf424758bd46955bb5a49815c4c4cb71765b0c
KiranHayer <39171857+KiranHayer@users.noreply.github.com>
2019-07-11 00:45:11 -0400

    Create app.js

---
+++ app.js
@@ -0,0 +1,123 @@
+var bodyParser = require("body-parser");
+methodOverride = require("method-override");
+mongoose       = require("mongoose");
+express        = require("express");
+app            = express();
+
+//APP CONFIG
+mongoose.connect("mongodb://localhost/restful_blog_app");
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+
+//MONGOOSE/MODEL CONFIG
+var blogSchema = new mongoose.Schema({
+	title: String,
+	image:  {
+    type: String,
+    default: "https://www.w3schools.com/howto/img_avatar2.png"
+  },
+	body: String,
+	created: {type: Date, default: Date.now}
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+//RESTFUL ROUTES
+app.get("/", function(req, res){
+	res.redirect("/blogs");
+});
+
+//INDEX ROUTE
+app.get("/blogs", function(req, res){
+	Blog.find({}, function(err, blogs){
+		if(err){
+			console.log("ERROR!");
+		}else{
+			res.render("index", {blogs: blogs});
+		}
+	});
+});
+
+//NEW ROUTE
+app.get("/blogs/new", function(req, res){
+ res.render("new");	
+});
+
+//CREATE ROUTE
+app.post("/blogs", function(req, res){
+	//create blog
+	Blog.create(req.body.blog, function(err, newBlog){
+	if(err){
+		res.render("new");
+	}else {
+		//then, redirect to the index 
+		res.redirect("/blogs");
+	}
+});
+});
+
+//SHOW ROUTE
+app.get("/blogs/:id", function(req, res){
+	Blog.findById(req.params.id, function(err, foundBlog){
+		if(err){
+			res.redirect("/blogs");
+		}else{
+			res.render("show", {blog: foundBlog});
+		}
+		res.send("SHOW PAGE!");
+		});
+});
+//EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res){
+	Blog.findById(req.params.id, function(err, foundBlog){
+	 if(err){
+		res.redirect("/blogs");
+	 } else {
+		res.render("edit", {blog: foundBlog});
+	}
+	});
+	
+});
+//UPDATE ROUTE
+app.put("/blogs/:id", function(req, res){
+	Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updateBlog){
+		if
+(err){
+	res.redirect("/blogs");
+} else{
+	res.redirect("/blogs/" + req.params.id);
+}
+	} );
+	//res.send("UPDATE ROUTE!");
+});
+
+//DELETE ROUTE
+app.delete("/blogs/:id", function(req, res){
+	Blog.findByIdAndRemove(req.params.id, function(err){
+		if(err){
+			res.redirect("/blogs");
+		}else{
+			res.redirect("/blogs");
+		}
+	});
+});
+//title 
+//image
+//body
+//created
+Blog.create({
+	title: "Test Blog",
+	image: "https://www.w3schools.com/howto/img_avatar2.png",
+	body: " HELLOO THIS IS A BLOG POST!"
+	
+});
+
+app.listen(3001, process.env.IP, function(){
+	console.log("SERVER IS RUNNING!");
+});
+
+
+
+
